dd40e867e188a2c87484f3399d5dd2155345dff2
Your Name <you@example.com>
2023-08-31 10:23:22 +0400

    login olmaq js ile

---
+++ registr.js
@@ -0,0 +1,35 @@
+var button=document.querySelector("#button")
+var h2=document.querySelector("#h2")
+var h3=document.querySelector("#h3")
+var name=document.querySelector("#input")
+var pasword=document.querySelector("#password")
+
+
+button.addEventListener("click",function(){
+    var value1=input.value;
+    var value2=password.value;
+    yoxlasana(value1,value2);
+
+   })
+function yoxlasana(value1,value2)  { 
+    const array=['1','2','3','4','5','6','7','8','9','0'];
+var array1=[]
+    array1=value1.split("");
+if (value1.length>=3 && value1.length<=8){
+ 
+for(i of array1){
+    if (array.includes(i)){
+ return alert('adinizi daxil etdikde reqemden istifade etmeyin')}
+}
+value2=value2.trim();
+if (value2.length>=8 && value2.length<=20){
+    return h2.innerHTML=` Name : ${input.value}` ,h3.innerHTML=`PASSWORD : ${password.value}`
+}
+
+else {
+  return alert(' sifrenizin sayi 8 den boyuk 20 den kicik olmalidir')
+}
+}
+else{
+    return  alert('adinizin uzunlugu 3 simvoldan boyuk 8 simvoldan kicik olmalidi')
+}}
